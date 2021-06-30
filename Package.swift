54cb4fd5d2f6e18cbf002b0112928a9d566a8d84
Gal-Yedidovich-iSrc <84014945+Gal-Yedidovich-iSrc@users.noreply.github.com>
2021-06-30 11:48:44 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-		.binaryTarget(name: "SuperApps", url: "https://github.com/maverick-studios/ios-sdk/releases/download/0.1.2/SuperApps.xcframework.zip", checksum: "ead7d06c03a7d50159cb27545fe795695923132912bd95b1b7ed9b33e1a7ff52"),
+		.binaryTarget(name: "SuperApps", url: "https://github.com/maverick-studios/ios-sdk/releases/download/0.1.3/SuperApps.xcframework.zip", checksum: "063cc6ea8c4cf23aa671baa5052a1862cd35490a658959fb4bdeca46856528d4"),
         .testTarget(
             name: "SuperAppsTests",
             dependencies: ["SuperApps"]),
